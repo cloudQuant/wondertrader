bd35a56289229de1ebc4d2e9e0818472fecb9fae
cloudQuant <yunjinqi@gmail.com>
2025-04-28 14:40:50 +0800

    add comments for WtDataWriter.h

--- src/WtDataStorage/WtDataWriter.h
+++ src/WtDataStorage/WtDataWriter.h
@@ -1,4 +1,14 @@
-﻿#pragma once
+/*!
+ * \file WtDataWriter.h
+ * \brief 数据写入器头文件
+ * \author Wesley
+ * \date 2020/03/30
+ * 
+ * \details
+ * 该文件定义了数据写入器的接口和实现，用于将行情数据写入到文件中
+ */
+
+#pragma once
 #include "DataDefine.h"
 
 #include "../Includes/FasterDefs.h"
@@ -10,6 +20,9 @@
 #include <queue>
 #include <map>
 
+/*!
+ * \brief Boost内存映射文件智能指针类型
+ */
 typedef std::shared_ptr<BoostMappingFile> BoostMFPtr;
 
 NS_WTP_BEGIN
@@ -19,62 +32,227 @@ NS_WTP_END
 
 USING_NS_WTP;
 
+/*!
+ * \brief 数据写入器类
+ * \details 
+ * 实现了IDataWriter接口，负责将行情数据写入到文件中
+ * 支持Tick数据、K线数据、委托队列、委托明细和成交明细等多种数据的写入
+ * 提供了异步处理和缓存机制，以提高数据处理效率
+ */
 class WtDataWriter : public IDataWriter
 {
 public:
+	/*!
+	 * \brief 构造函数
+	 */
 	WtDataWriter();
+
+	/*!
+	 * \brief 析构函数
+	 */
 	~WtDataWriter();	
 
 private:
+	/*!
+	 * \brief 调整实时数据块大小
+	 * \tparam HeaderType 数据块头部类型
+	 * \tparam T 数据类型
+	 * \param mfPtr 内存映射文件指针
+	 * \param nCount 需要的数据项数量
+	 * \return 调整后的数据块指针
+	 * 
+	 * \details 根据需要的数据项数量调整内存映射文件的大小
+	 */
 	template<typename HeaderType, typename T>
 	void* resizeRTBlock(BoostMFPtr& mfPtr, uint32_t nCount);
 
+	/*!
+	 * \brief 处理循环
+	 * 
+	 * \details 异步处理数据的工作线程函数，从任务队列中获取并处理数据
+	 */
 	void  proc_loop();
 
+	/*!
+	 * \brief 检查循环
+	 * 
+	 * \details 定期检查并处理历史数据的工作线程函数
+	 */
 	void  check_loop();
 
+	/*!
+	 * \brief 将K线数据写入文件
+	 * \param ct 合约信息指针
+	 * \return 写入的数据数量
+	 * 
+	 * \details 将合约的K线数据写入到对应的文件中
+	 */
 	uint32_t  dump_bars_to_file(WTSContractInfo* ct);
 
+	/*!
+	 * \brief 通过数据转储器将K线数据写入
+	 * \param ct 合约信息指针
+	 * \return 写入的数据数量
+	 * 
+	 * \details 通过外部数据转储器将合约的K线数据写入
+	 */
 	uint32_t  dump_bars_via_dumper(WTSContractInfo* ct);
 
 private:
+	/*!
+	 * \brief 写入日线数据
+	 * \param ct 合约信息指针
+	 * \param newBar 新的日线K线数据
+	 * \return 是否成功写入
+	 * 
+	 * \details 将合约的日线数据写入到文件中
+	 */
 	bool	dump_day_data(WTSContractInfo* ct, WTSBarStruct* newBar);
 
+	/*!
+	 * \brief 处理数据块内容
+	 * \param tag 标签，用于日志输出
+	 * \param content 数据块内容，传入传出参数
+	 * \param isBar 是否为K线数据
+	 * \param bKeepHead 是否保留数据块头部
+	 * \return 是否成功处理
+	 * 
+	 * \details 处理数据块的压缩和版本转换
+	 */
 	bool	proc_block_data(const char* tag, std::string& content, bool isBar, bool bKeepHead = true);
 
+	/*!
+	 * \brief 处理Tick数据
+	 * \param curTick 当前Tick数据指针
+	 * \param procFlag 处理标志
+	 * 
+	 * \details 处理Tick数据，包括更新缓存、写入文件和生成K线等
+	 */
 	void	procTick(WTSTickData* curTick, uint32_t procFlag);
+
+	/*!
+	 * \brief 处理委托队列数据
+	 * \param curOrdQue 当前委托队列数据指针
+	 * 
+	 * \details 处理委托队列数据，写入到对应的文件中
+	 */
 	void	procQueue(WTSOrdQueData* curOrdQue);
+
+	/*!
+	 * \brief 处理委托明细数据
+	 * \param curOrdDetail 当前委托明细数据指针
+	 * 
+	 * \details 处理委托明细数据，写入到对应的文件中
+	 */
 	void	procOrder(WTSOrdDtlData* curOrdDetail);
+
+	/*!
+	 * \brief 处理成交数据
+	 * \param curTrans 当前成交数据指针
+	 * 
+	 * \details 处理成交数据，写入到对应的文件中
+	 */
 	void	procTrans(WTSTransData* curTrans);
 
 public:
+	/*!
+	 * \brief 初始化数据写入器
+	 * \param params 配置参数
+	 * \param sink 数据写入器回调接口
+	 * \return 是否初始化成功
+	 * 
+	 * \details 根据配置参数初始化数据写入器，设置目录和各种处理标志
+	 */
 	virtual bool init(WTSVariant* params, IDataWriterSink* sink) override;
+	/*!
+	 * \brief 释放数据写入器资源
+	 * 
+	 * \details 清理并释放数据写入器占用的所有资源
+	 */
 	virtual void release() override;
 
+	/*!
+	 * \brief 写入Tick数据
+	 * \param curTick 当前Tick数据指针
+	 * \param procFlag 处理标志
+	 * \return 是否成功写入
+	 * 
+	 * \details 将Tick数据写入到文件中，并根据标志进行相应的处理
+	 */
 	virtual bool writeTick(WTSTickData* curTick, uint32_t procFlag) override;
 
+	/*!
+	 * \brief 写入委托队列数据
+	 * \param curOrdQue 当前委托队列数据指针
+	 * \return 是否成功写入
+	 * 
+	 * \details 将委托队列数据写入到文件中
+	 */
 	virtual bool writeOrderQueue(WTSOrdQueData* curOrdQue) override;
 
+	/*!
+	 * \brief 写入委托明细数据
+	 * \param curOrdDetail 当前委托明细数据指针
+	 * \return 是否成功写入
+	 * 
+	 * \details 将委托明细数据写入到文件中
+	 */
 	virtual bool writeOrderDetail(WTSOrdDtlData* curOrdDetail) override;
 
+	/*!
+	 * \brief 写入成交数据
+	 * \param curTrans 当前成交数据指针
+	 * \return 是否成功写入
+	 * 
+	 * \details 将成交数据写入到文件中
+	 */
 	virtual bool writeTransaction(WTSTransData* curTrans) override;
 
+	/*!
+	 * \brief 转换历史数据
+	 * \param sid 交易日ID
+	 * 
+	 * \details 将指定交易日的实时数据转换为历史数据
+	 */
 	virtual void transHisData(const char* sid) override;
 	
+	/*!
+	 * \brief 检查交易日是否已处理
+	 * \param sid 交易日ID
+	 * \return 是否已处理
+	 * 
+	 * \details 检查指定交易日的数据是否已经转换处理
+	 */
 	virtual bool isSessionProceeded(const char* sid) override;
 
+	/*!
+	 * \brief 获取当前Tick数据
+	 * \param code 合约代码
+	 * \param exchg 交易所代码，默认为空
+	 * \return 当前Tick数据指针
+	 * 
+	 * \details 根据合约代码和交易所获取当前缓存的Tick数据
+	 */
 	virtual WTSTickData* getCurTick(const char* code, const char* exchg = "") override;
 
 private:
-	IBaseDataMgr*		_bd_mgr;
+	IBaseDataMgr*		_bd_mgr;			///< 基础数据管理器指针
 
+	/*!
+	 * \brief K线数据块对结构
+	 * \details 用于管理K线数据块和相关的内存映射文件
+	 */
 	typedef struct _KBlockPair
 	{
-		RTKlineBlock*	_block;
-		BoostMFPtr		_file;
-		SpinMutex		_mutex;
-		uint64_t		_lasttime;
+		RTKlineBlock*	_block;			///< K线数据块指针
+		BoostMFPtr		_file;			///< 内存映射文件指针
+		SpinMutex		_mutex;			///< 自旋锁，用于线程同步
+		uint64_t		_lasttime;		///< 最后更新时间
 
+		/*!
+		 * \brief 构造函数
+		 * \details 初始化成员变量
+		 */
 		_KBlockPair()
 		{
 			_block = NULL;
@@ -85,15 +263,23 @@ private:
 	} KBlockPair;
 	typedef wt_hashmap<std::string, KBlockPair*>	KBlockFilesMap;
 
+	/*!
+	 * \brief Tick数据块对结构
+	 * \details 用于管理Tick数据块和相关的内存映射文件
+	 */
 	typedef struct _TickBlockPair
 	{
-		RTTickBlock*	_block;
-		BoostMFPtr		_file;
-		SpinMutex		_mutex;
-		uint64_t		_lasttime;
+		RTTickBlock*	_block;			///< Tick数据块指针
+		BoostMFPtr		_file;			///< 内存映射文件指针
+		SpinMutex		_mutex;			///< 自旋锁，用于线程同步
+		uint64_t		_lasttime;		///< 最后更新时间
 
-		std::shared_ptr< std::ofstream>	_fstream;
+		std::shared_ptr< std::ofstream>	_fstream;	///< 文件流指针，用于写入日志
 
+		/*!
+		 * \brief 构造函数
+		 * \details 初始化成员变量
+		 */
 		_TickBlockPair()
 		{
 			_block = NULL;
@@ -104,13 +290,21 @@ private:
 	} TickBlockPair;
 	typedef wt_hashmap<std::string, TickBlockPair*>	TickBlockFilesMap;
 
+	/*!
+	 * \brief 成交数据块对结构
+	 * \details 用于管理成交数据块和相关的内存映射文件
+	 */
 	typedef struct _TransBlockPair
 	{
-		RTTransBlock*	_block;
-		BoostMFPtr		_file;
-		SpinMutex		_mutex;
-		uint64_t		_lasttime;
+		RTTransBlock*	_block;			///< 成交数据块指针
+		BoostMFPtr		_file;			///< 内存映射文件指针
+		SpinMutex		_mutex;			///< 自旋锁，用于线程同步
+		uint64_t		_lasttime;		///< 最后更新时间
 
+		/*!
+		 * \brief 构造函数
+		 * \details 初始化成员变量
+		 */
 		_TransBlockPair()
 		{
 			_block = NULL;
@@ -120,13 +314,21 @@ private:
 	} TransBlockPair;
 	typedef wt_hashmap<std::string, TransBlockPair*>	TransBlockFilesMap;
 
+	/*!
+	 * \brief 委托明细数据块对结构
+	 * \details 用于管理委托明细数据块和相关的内存映射文件
+	 */
 	typedef struct _OdeDtlBlockPair
 	{
-		RTOrdDtlBlock*	_block;
-		BoostMFPtr		_file;
-		SpinMutex		_mutex;
-		uint64_t		_lasttime;
+		RTOrdDtlBlock*	_block;			///< 委托明细数据块指针
+		BoostMFPtr		_file;			///< 内存映射文件指针
+		SpinMutex		_mutex;			///< 自旋锁，用于线程同步
+		uint64_t		_lasttime;		///< 最后更新时间
 
+		/*!
+		 * \brief 构造函数
+		 * \details 初始化成员变量
+		 */
 		_OdeDtlBlockPair()
 		{
 			_block = NULL;
@@ -136,13 +338,21 @@ private:
 	} OrdDtlBlockPair;
 	typedef wt_hashmap<std::string, OrdDtlBlockPair*>	OrdDtlBlockFilesMap;
 
+	/*!
+	 * \brief 委托队列数据块对结构
+	 * \details 用于管理委托队列数据块和相关的内存映射文件
+	 */
 	typedef struct _OdeQueBlockPair
 	{
-		RTOrdQueBlock*	_block;
-		BoostMFPtr		_file;
-		SpinMutex		_mutex;
-		uint64_t		_lasttime;
+		RTOrdQueBlock*	_block;			///< 委托队列数据块指针
+		BoostMFPtr		_file;			///< 内存映射文件指针
+		SpinMutex		_mutex;			///< 自旋锁，用于线程同步
+		uint64_t		_lasttime;		///< 最后更新时间
 
+		/*!
+		 * \brief 构造函数
+		 * \details 初始化成员变量
+		 */
 		_OdeQueBlockPair()
 		{
 			_block = NULL;
@@ -153,91 +363,197 @@ private:
 	typedef wt_hashmap<std::string, OrdQueBlockPair*>	OrdQueBlockFilesMap;
 	
 
-	KBlockFilesMap	_rt_min1_blocks;
-	KBlockFilesMap	_rt_min5_blocks;
+	KBlockFilesMap	_rt_min1_blocks;		///< 1分钟线K线数据块映射表
+	KBlockFilesMap	_rt_min5_blocks;		///< 5分钟线K线数据块映射表
 
-	TickBlockFilesMap	_rt_ticks_blocks;
-	TransBlockFilesMap	_rt_trans_blocks;
-	OrdDtlBlockFilesMap _rt_orddtl_blocks;
-	OrdQueBlockFilesMap _rt_ordque_blocks;
+	TickBlockFilesMap	_rt_ticks_blocks;		///< Tick数据块映射表
+	TransBlockFilesMap	_rt_trans_blocks;		///< 成交数据块映射表
+	OrdDtlBlockFilesMap _rt_orddtl_blocks;		///< 委托明细数据块映射表
+	OrdQueBlockFilesMap _rt_ordque_blocks;		///< 委托队列数据块映射表
 
-	SpinMutex		_lck_tick_cache;
-	wt_hashmap<std::string, uint32_t> _tick_cache_idx;
-	BoostMFPtr		_tick_cache_file;
-	RTTickCache*	_tick_cache_block;
+	SpinMutex		_lck_tick_cache;			///< Tick缓存锁
+	wt_hashmap<std::string, uint32_t> _tick_cache_idx;	///< Tick缓存索引表
+	BoostMFPtr		_tick_cache_file;			///< Tick缓存文件
+	RTTickCache*	_tick_cache_block;			///< Tick缓存数据块
 
 	//typedef std::function<void()> TaskInfo;
+	/*!
+	 * \brief 任务信息结构
+	 * \details 用于存储异步处理的数据任务信息，对齐到64字节以提高缓存效率
+	 */
 	typedef struct alignas(64) _TaskInfo
 	{
-		WTSObject*	_obj;
-		uint64_t	_type;
-		uint32_t	_flag;		
+		WTSObject*	_obj;			///< 数据对象指针
+		uint64_t	_type;			///< 任务类型
+		uint32_t	_flag;			///< 处理标志
 
+		/*!
+		 * \brief 构造函数
+		 * \param data 数据对象指针
+		 * \param dtype 数据类型
+		 * \param flag 处理标志，默认为0
+		 */
 		_TaskInfo(WTSObject* data, uint64_t dtype, uint32_t flag = 0);
 
+		/*!
+		 * \brief 拷贝构造函数
+		 * \param rhs 源任务信息对象
+		 */
 		_TaskInfo(const _TaskInfo& rhs);
 
+		/*!
+		 * \brief 析构函数
+		 */
 		~_TaskInfo();
 
 	} TaskInfo;
-	std::queue<TaskInfo>	_tasks;
-	StdThreadPtr			_task_thrd;
-	StdUniqueMutex			_task_mtx;
-	StdCondVariable			_task_cond;
+	std::queue<TaskInfo>	_tasks;				///< 任务队列
+	StdThreadPtr			_task_thrd;			///< 任务处理线程
+	StdUniqueMutex			_task_mtx;			///< 任务队列互斥锁
+	StdCondVariable			_task_cond;			///< 任务队列条件变量
 
-	std::string		_base_dir;
-	std::string		_cache_file;
-	uint32_t		_log_group_size;
-	bool			_async_proc;
+	std::string		_base_dir;				///< 基础目录
+	std::string		_cache_file;				///< 缓存文件路径
+	uint32_t		_log_group_size;			///< 日志分组大小
+	bool			_async_proc;				///< 是否异步处理
 
-	StdCondVariable	_proc_cond;
-	StdUniqueMutex	_proc_mtx;
-	std::queue<std::string> _proc_que;
-	StdThreadPtr	_proc_thrd;
-	StdThreadPtr	_proc_chk;
-	bool			_terminated;
+	StdCondVariable	_proc_cond;				///< 处理队列条件变量
+	StdUniqueMutex	_proc_mtx;				///< 处理队列互斥锁
+	std::queue<std::string> _proc_que;			///< 处理队列
+	StdThreadPtr	_proc_thrd;				///< 处理线程
+	StdThreadPtr	_proc_chk;				///< 检查线程
+	bool			_terminated;				///< 是否终止标志
 
-	bool			_save_tick_log;
-	bool			_skip_notrade_tick;
-	bool			_skip_notrade_bar;
-	bool			_disable_his;
+	bool			_save_tick_log;				///< 是否保存Tick日志
+	bool			_skip_notrade_tick;			///< 是否跳过非交易的Tick
+	bool			_skip_notrade_bar;			///< 是否跳过非交易的K线
+	bool			_disable_his;				///< 是否禁用历史数据处理
 
-	bool			_disable_tick;
-	bool			_disable_min1;
-	bool			_disable_min5;
-	bool			_disable_day;
+	bool			_disable_tick;				///< 是否禁用Tick数据处理
+	bool			_disable_min1;				///< 是否禁用1分钟线处理
+	bool			_disable_min5;				///< 是否禁用5分钟线处理
+	bool			_disable_day;				///< 是否禁用日线处理
 
-	bool			_disable_trans;
-	bool			_disable_ordque;
-	bool			_disable_orddtl;
+	bool			_disable_trans;				///< 是否禁用成交数据处理
+	bool			_disable_ordque;				///< 是否禁用委托队列处理
+	bool			_disable_orddtl;				///< 是否禁用委托明细处理
 
 	/*
 	 *	by Wesley @ 2023.05.04
 	 *	分钟线价格模式，0-常规模式，1-将买卖价也记录下来，这个设计时只针对期权这种不活跃的品种
 	 */
-	uint32_t		_min_price_mode;
+	uint32_t		_min_price_mode;			///< 分钟线价格模式，0-常规模式，1-将买卖价也记录下来
 	
-	std::map<std::string, uint32_t> _proc_date;
+	std::map<std::string, uint32_t> _proc_date;	///< 已处理的交易日映射表
 
 private:
+	/*!
+	 * \brief 加载Tick缓存
+	 * 
+	 * \details 从缓存文件中加载最新的Tick数据到内存中
+	 */
 	void loadCache();
 
+	/*!
+	 * \brief 更新Tick缓存
+	 * \param ct 合约信息指针
+	 * \param curTick 当前Tick数据指针
+	 * \param procFlag 处理标志
+	 * \return 是否成功更新缓存
+	 * 
+	 * \details 将最新的Tick数据更新到缓存中
+	 */
 	bool updateCache(WTSContractInfo* ct, WTSTickData* curTick, uint32_t procFlag);
 
+	/*!
+	 * \brief 将Tick数据写入到实时数据文件
+	 * \param ct 合约信息指针
+	 * \param curTick 当前Tick数据指针
+	 * 
+	 * \details 将Tick数据写入到对应的实时数据文件中
+	 */
 	void pipeToTicks(WTSContractInfo* ct, WTSTickData* curTick);
 
+	/*!
+	 * \brief 根据Tick数据生成并写入K线数据
+	 * \param ct 合约信息指针
+	 * \param curTick 当前Tick数据指针
+	 * 
+	 * \details 根据Tick数据生成分钟线K线数据，并写入到对应的文件中
+	 */
 	void pipeToKlines(WTSContractInfo* ct, WTSTickData* curTick);
 
+	/*!
+	 * \brief 获取K线数据块
+	 * \param ct 合约信息指针
+	 * \param period K线周期
+	 * \param bAutoCreate 如果不存在是否自动创建，默认为true
+	 * \return K线数据块对指针
+	 * 
+	 * \details 根据合约信息和K线周期获取对应的K线数据块
+	 */
 	KBlockPair* getKlineBlock(WTSContractInfo* ct, WTSKlinePeriod period, bool bAutoCreate = true);
 
+	/*!
+	 * \brief 获取Tick数据块
+	 * \param ct 合约信息指针
+	 * \param curDate 当前日期
+	 * \param bAutoCreate 如果不存在是否自动创建，默认为true
+	 * \return Tick数据块对指针
+	 * 
+	 * \details 根据合约信息和日期获取对应的Tick数据块
+	 */
 	TickBlockPair* getTickBlock(WTSContractInfo* ct, uint32_t curDate, bool bAutoCreate = true);
+
+	/*!
+	 * \brief 获取成交数据块
+	 * \param ct 合约信息指针
+	 * \param curDate 当前日期
+	 * \param bAutoCreate 如果不存在是否自动创建，默认为true
+	 * \return 成交数据块对指针
+	 * 
+	 * \details 根据合约信息和日期获取对应的成交数据块
+	 */
 	TransBlockPair* getTransBlock(WTSContractInfo* ct, uint32_t curDate, bool bAutoCreate = true);
+
+	/*!
+	 * \brief 获取委托明细数据块
+	 * \param ct 合约信息指针
+	 * \param curDate 当前日期
+	 * \param bAutoCreate 如果不存在是否自动创建，默认为true
+	 * \return 委托明细数据块对指针
+	 * 
+	 * \details 根据合约信息和日期获取对应的委托明细数据块
+	 */
 	OrdDtlBlockPair* getOrdDtlBlock(WTSContractInfo* ct, uint32_t curDate, bool bAutoCreate = true);
+
+	/*!
+	 * \brief 获取委托队列数据块
+	 * \param ct 合约信息指针
+	 * \param curDate 当前日期
+	 * \param bAutoCreate 如果不存在是否自动创建，默认为true
+	 * \return 委托队列数据块对指针
+	 * 
+	 * \details 根据合约信息和日期获取对应的委托队列数据块
+	 */
 	OrdQueBlockPair* getOrdQueBlock(WTSContractInfo* ct, uint32_t curDate, bool bAutoCreate = true);
 
+	/*!
+	 * \brief 释放数据块资源
+	 * \tparam T 数据块类型
+	 * \param block 数据块指针
+	 * 
+	 * \details 释放指定类型的数据块资源
+	 */
 	template<typename T>
 	void	releaseBlock(T* block);
 
+	/*!
+	 * \brief 将任务添加到任务队列
+	 * \param task 任务信息
+	 * 
+	 * \details 将数据处理任务添加到异步处理队列中
+	 */
 	void pushTask(const TaskInfo& task);
 };
 
