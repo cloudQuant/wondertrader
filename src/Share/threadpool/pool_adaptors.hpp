c8daaad92805d5ec9dd08c6b33fd68362df5012d
cloudQuant <yunjinqi@gmail.com>
2025-05-05 22:19:34 +0800

    add comments threadpool

--- src/Share/threadpool/pool_adaptors.hpp
+++ src/Share/threadpool/pool_adaptors.hpp
@@ -1,13 +1,13 @@
-/*! \file
-* \brief Pool adaptors.
+/*! \file pool_adaptors.hpp
+* \brief 线程池适配器
 *
-* This file contains an easy-to-use adaptor similar to a smart 
-* pointer for the pool class. 
+* \details 本文件包含了线程池的易用适配器，类似于智能指针的使用方式。
+* 提供了多个便捷的 schedule 函数重载，简化了任务调度过程。
 *
 * Copyright (c) 2005-2007 Philipp Henkel
 *
-* Use, modification, and distribution are  subject to the
-* Boost Software License, Version 1.0. (See accompanying  file
+* Use, modification, and distribution are subject to the
+* Boost Software License, Version 1.0. (See accompanying file
 * LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 *
 * http://threadpool.sourceforge.net
@@ -25,22 +25,35 @@ namespace boost { namespace threadpool
 {
 
 
-// TODO convenience scheduling function
-    /*! Schedules a Runnable for asynchronous execution. A Runnable is an arbitrary class with a run()
-    * member function. This a convenience shorthand for pool->schedule(bind(&Runnable::run, task_object)).
-    * \param 
-    * \param obj The Runnable object. The member function run() will be exectued and should not throw execeptions.
-    * \return true, if the task could be scheduled and false otherwise. 
-    */  
+    /**
+     * @brief 将一个 Runnable 对象调度到线程池中异步执行
+     * 
+     * @details Runnable 是一个拥有 run() 成员函数的任意类。这是 pool->schedule(bind(&Runnable::run, task_object)) 的便捷写法。
+     * 通过这个函数，可以直接将具有 run() 成员函数的对象提交到线程池中。
+     * 
+     * @tparam Pool 线程池类型
+     * @tparam Runnable 可运行对象类型，需要拥有 run() 成员函数
+     * @param pool 线程池实例
+     * @param obj 可运行对象，其 run() 成员函数将被执行，该函数不应该抛出异常
+     * @return 如果任务成功调度则返回 true，否则返回 false
+     */  
     template<typename Pool, typename Runnable>
     bool schedule(Pool& pool, shared_ptr<Runnable> const & obj)
     {	
       return pool->schedule(bind(&Runnable::run, obj));
     }	
     
-    /*! Schedules a task for asynchronous execution. The task will be executed once only.
-    * \param task The task function object.
-    */  
+    /**
+     * @brief 将任务函数对象提交到线程池中异步执行
+     * 
+     * @details 任务将被执行一次。这个函数重载直接接受池实例引用。
+     * 注意这个版本只适用于返回类型为 void 的任务函数。
+     * 
+     * @tparam Pool 线程池类型
+     * @param pool 线程池实例引用
+     * @param task 要执行的任务函数对象
+     * @return 如果任务成功调度则返回 true，否则返回 false
+     */  
     template<typename Pool>
     typename enable_if < 
       is_void< typename result_of< typename Pool::task_type() >::type >,
@@ -52,6 +65,18 @@ namespace boost { namespace threadpool
     }	
 
 
+    /**
+     * @brief 将任务函数对象提交到线程池中异步执行（智能指针版本）
+     * 
+     * @details 任务将被执行一次。这个函数重载接受指向池实例的智能指针。
+     * 便于在使用智能指针管理线程池的场景下使用。
+     * 注意这个版本只适用于返回类型为 void 的任务函数。
+     * 
+     * @tparam Pool 线程池类型
+     * @param pool 指向线程池实例的智能指针
+     * @param task 要执行的任务函数对象
+     * @return 如果任务成功调度则返回 true，否则返回 false
+     */
     template<typename Pool>
     typename enable_if < 
       is_void< typename result_of< typename Pool::task_type() >::type >,
