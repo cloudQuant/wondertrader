1da5fa22a99f998bc2de1faa6dda51ba1d6e98b9
cloudQuant <yunjinqi@gmail.com>
2025-05-06 14:51:36 +0800

    WtDistExecuter

--- src/WtCore/WtDistExecuter.cpp
+++ src/WtCore/WtDistExecuter.cpp
@@ -1,4 +1,15 @@
-﻿#include "WtDistExecuter.h"
+/*!
+ * \file WtDistExecuter.cpp
+ * \project	WonderTrader
+ *
+ * \author Wesley
+ * \date 2020/03/30
+ * 
+ * \brief 分布式执行器实现
+ * \details 实现分布式执行器的核心功能，包括初始化、目标仓位设置和通知
+ */
+
+#include "WtDistExecuter.h"
 
 #include "../Includes/WTSVariant.hpp"
 
@@ -7,17 +18,32 @@
 
 USING_NS_WTP;
 
+/**
+ * \brief 分布式执行器构造函数
+ * \details 初始化分布式执行器，调用父类构造函数
+ * \param name 执行器名称
+ */
 WtDistExecuter::WtDistExecuter(const char* name)
 	: IExecCommand(name)
 {
 
 }
 
+/**
+ * \brief 分布式执行器析构函数
+ * \details 资源清理
+ */
 WtDistExecuter::~WtDistExecuter()
 {
 
 }
 
+/**
+ * \brief 初始化分布式执行器
+ * \details 从配置参数中读取如缩放比例等设置
+ * \param params 配置参数集合
+ * \return 是否初始化成功
+ */
 bool WtDistExecuter::init(WTSVariant* params)
 {
 	if (params == NULL)
@@ -31,6 +57,11 @@ bool WtDistExecuter::init(WTSVariant* params)
 	return true;
 }
 
+/**
+ * \brief 设置目标仓位
+ * \details 批量更新多个品种的目标仓位，并广播变化
+ * \param targets 目标仓位映射表，键为品种代码，值为目标仓位
+ */
 void WtDistExecuter::set_position(const wt_hashmap<std::string, double>& targets)
 {
 	for (auto it = targets.begin(); it != targets.end(); it++)
@@ -38,6 +69,7 @@ void WtDistExecuter::set_position(const wt_hashmap<std::string, double>& targets
 		const char* stdCode = it->first.c_str();
 		double newVol = it->second;
 
+		// 应用仓位缩放比例
 		newVol *= _scale;
 		double oldVol = _target_pos[stdCode];
 		_target_pos[stdCode] = newVol;
@@ -46,12 +78,19 @@ void WtDistExecuter::set_position(const wt_hashmap<std::string, double>& targets
 			WTSLogger::log_dyn("executer", _name.c_str(), LL_INFO, "[{}]{}目标仓位更新: {} -> {}", _name.c_str(), stdCode, oldVol, newVol);
 		}
 
-		//这里广播目标仓位
+		// TODO: 这里广播目标仓位到其他节点
 	}
 }
 
+/**
+ * \brief 处理单个品种的目标仓位变化
+ * \details 更新单个品种的目标仓位，并广播到其他节点
+ * \param stdCode 品种代码
+ * \param targetPos 目标仓位
+ */
 void WtDistExecuter::on_position_changed(const char* stdCode, double targetPos)
 {
+	// 应用仓位缩放比例
 	targetPos *= _scale;
 
 	double oldVol = _target_pos[stdCode];
@@ -62,10 +101,16 @@ void WtDistExecuter::on_position_changed(const char* stdCode, double targetPos)
 		WTSLogger::log_dyn("executer", _name.c_str(), LL_INFO, "[{}]{}目标仓位更新: {} -> {}", _name.c_str(), stdCode, oldVol, targetPos);
 	}
 
-	//这里广播目标仓位
+	// TODO: 这里广播目标仓位到其他节点
 }
 
+/**
+ * \brief 处理行情数据回调
+ * \details 分布式执行器不需要直接处理行情数据，这个方法是接口实现的空方法
+ * \param stdCode 品种代码
+ * \param newTick 最新的行情数据
+ */
 void WtDistExecuter::on_tick(const char* stdCode, WTSTickData* newTick)
 {
-	//分布式执行器不需要处理ontick
+	// 分布式执行器不需要处理ontick
 }
--- src/WtCore/WtDistExecuter.h
+++ src/WtCore/WtDistExecuter.h
@@ -1,39 +1,91 @@
-﻿#pragma once
+/*!
+ * \file WtDistExecuter.h
+ * \project	WonderTrader
+ *
+ * \author Wesley
+ * \date 2020/03/30
+ * 
+ * \brief 分布式执行器头文件
+ * \details 分布式执行器用于在分布式环境下广播目标仓位信息，
+ *          与差量执行器不同，它不直接执行交易，而是将目标仓位广播给其他节点
+ */
+#pragma once
 #include "IExecCommand.h"
 
 NS_WTP_BEGIN
 class WTSVariant;
 
+/**
+ * \brief 分布式执行器类
+ * \details 用于在分布式系统中管理和广播目标仓位信息，但不直接执行交易操作
+ *          继承自IExecCommand接口，实现了执行命令的基本功能
+ */
 class WtDistExecuter : public IExecCommand
 {
 public:
+	/**
+	 * \brief 构造函数
+	 * \param name 执行器名称
+	 */
 	WtDistExecuter(const char* name);
+
+	/**
+	 * \brief 析构函数
+	 */
 	virtual ~WtDistExecuter();
 
 public:
-	/*
-	 *	初始化执行器
-	 *	传入初始化参数
+	/**
+	 * \brief 初始化执行器
+	 * \param params 初始化参数集合，包含如scale等参数
+	 * \return 是否初始化成功
 	 */
 	bool init(WTSVariant* params);
 
 
 public:
 	//////////////////////////////////////////////////////////////////////////
-	//IExecCommand
+	// IExecCommand接口实现
+	/**
+	 * \brief 设置目标仓位
+	 * \details 批量设置多个标的仓位，并广播这些仓位信息
+	 * \param targets 目标仓位映射表，键为标的代码，值为目标仓位
+	 */
 	virtual void set_position(const wt_hashmap<std::string, double>& targets) override;
 
-
+	/**
+	 * \brief 单个品种目标仓位变化通知
+	 * \details 当单个品种的目标仓位发生变化时触发，用于广播目标仓位变化
+	 * \param stdCode 品种代码
+	 * \param targetPos 目标仓位
+	 */
 	virtual void on_position_changed(const char* stdCode, double targetPos) override;
 
-
+	/**
+	 * \brief 行情数据回调
+	 * \details 在分布式执行器中不处理行情数据
+	 * \param stdCode 品种代码
+	 * \param newTick 最新的行情数据
+	 */
 	virtual void on_tick(const char* stdCode, WTSTickData* newTick) override;
 
 private:
+	/**
+	 * \brief 配置项
+	 * \details 存储分布式执行器的配置参数
+	 */
 	WTSVariant*			_config;
 
+	/**
+	 * \brief 仓位缩放比例
+	 * \details 用于将输入的仓位数量按照比例放大或缩小
+	 */
 	uint32_t			_scale;
 
+	/**
+	 * \brief 目标仓位映射表
+	 * \details 存储所有品种的目标仓位情况，键为品种代码，值为目标仓位
+	 */
 	wt_hashmap<std::string, double> _target_pos;
 };
 NS_WTP_END
