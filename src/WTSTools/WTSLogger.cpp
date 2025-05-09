dfb19671ff2da2c6182d7b12e00231adaab83f9a
cloudQuant <yunjinqi@gmail.com>
2025-05-09 11:26:50 +0800

    WTSLogger

--- src/WTSTools/WTSLogger.cpp
+++ src/WTSTools/WTSLogger.cpp
@@ -1,11 +1,24 @@
-﻿/*!
+/*!
  * \file WTSLogger.cpp
  * \project	WonderTrader
  *
  * \author Wesley
  * \date 2020/03/30
  * 
- * \brief 
+ * \brief 日志模块实现
+ *
+ * 本文件实现了WonderTrader的日志系统，基于spdlog实现
+ * 支持各种日志级别、日志输出目标的配置与处理
+ * 实现了日志初始化、格式化输出、多级别日志控制等功能
+ * 
+ * 主要功能包括：
+ * 1. 支持多种日志级别：调试、信息、警告、错误和致命错误
+ * 2. 支持多种输出目标：控制台、文件（日常文件和基本文件）
+ * 3. 支持按分类输出日志，可以为不同模块设置不同的日志配置
+ * 4. 支持动态日志创建和格式化，可根据需要动态创建不同模式的日志器
+ * 5. 支持自定义日志处理器，可以扩展日志处理方式
+ * 6. 支持异步日志输出，提高性能
+ * 7. 支持线程安全的日志操作
  */
 #include <stdio.h>
 #include <iostream>
@@ -44,7 +57,12 @@ WTSLogger::LogPatterns*	WTSLogger::m_mapPatterns = NULL;
 thread_local char	WTSLogger::m_buffer[];
 std::set<std::string>	WTSLogger::m_setDynLoggers;
 
-inline spdlog::level::level_enum str_to_level( const char* slvl)
+/**
+ * @brief 将字符串格式的日志级别转换为spdlog日志级别枚举值
+ * @param slvl 日志级别字符串，可以是"debug", "info", "warn", "error", "fatal"
+ * @return spdlog::level::level_enum 返回spdlog对应的日志级别枚举值
+ */
+inline spdlog::level::level_enum str_to_level(const char* slvl)
 {
 	if(wt_stricmp(slvl, "debug") == 0)
 	{
@@ -72,6 +90,11 @@ inline spdlog::level::level_enum str_to_level( const char* slvl)
 	}
 }
 
+/**
+ * @brief 将字符串格式的日志级别转换为WTSLogLevel枚举值
+ * @param slvl 日志级别字符串，可以是"debug", "info", "warn", "error", "fatal"
+ * @return WTSLogLevel 返回WonderTrader系统内部对应的日志级别枚举值
+ */
 inline WTSLogLevel str_to_ll(const char* slvl)
 {
 	if (wt_stricmp(slvl, "debug") == 0)
@@ -100,6 +123,10 @@ inline WTSLogLevel str_to_ll(const char* slvl)
 	}
 }
 
+/**
+ * @brief 检查并创建文件路径中的目录
+ * @param filename 文件路径，如果路径中的目录不存在则创建
+ */
 inline void checkDirs(const char* filename)
 {
 	std::string s = StrUtil::standardisePath(filename, false);
@@ -114,6 +141,10 @@ inline void checkDirs(const char* filename)
 		boost::filesystem::create_directories(s.substr(0, pos).c_str());
 }
 
+/**
+ * @brief 输出当前时间标签
+ * @param bWithSpace 是否在时间标签后追加空格，默认为true
+ */
 inline void print_timetag(bool bWithSpace = true)
 {
 	uint64_t now = TimeUtils::getLocalTimeNow();
@@ -125,6 +156,10 @@ inline void print_timetag(bool bWithSpace = true)
 		fmt::print(" ");
 }
 
+/**
+ * @brief 直接打印日志消息到控制台
+ * @param buffer 要打印的消息内容
+ */
 void WTSLogger::print_message(const char* buffer)
 {
 	print_timetag(true);
@@ -132,6 +167,11 @@ void WTSLogger::print_message(const char* buffer)
 	fmt::print("\r\n");
 }
 
+/**
+ * @brief 初始化指定分类的日志器
+ * @param catName 分类名称，用于识别和管理日志器
+ * @param cfgLogger 日志器的配置信息，包含异步设置、日志级别、输出目标等
+ */
 void WTSLogger::initLogger(const char* catName, WTSVariant* cfgLogger)
 {
 	bool bAsync = cfgLogger->getBoolean("async");
@@ -200,6 +240,12 @@ void WTSLogger::initLogger(const char* catName, WTSVariant* cfgLogger)
 	}
 }
 
+/**
+ * @brief 初始化日志系统
+ * @param propFile 配置文件路径或配置内容字符串，默认为"logcfg.json"
+ * @param isFile 是否为文件路径，为false时propFile被视为配置内容字符串
+ * @param handler 自定义日志处理器，默认为NULL
+ */
 void WTSLogger::init(const char* propFile /* = "logcfg.json" */, bool isFile /* = true */, ILogHandler* handler /* = NULL */)
 {
 	if (m_bInited)
@@ -246,11 +292,20 @@ void WTSLogger::init(const char* propFile /* = "logcfg.json" */, bool isFile /* 
 	m_bInited = true;
 }
 
+/**
+ * @brief 注册自定义日志处理器
+ * @param handler 日志处理器实例，默认为NULL
+ */
 void WTSLogger::registerHandler(ILogHandler* handler /* = NULL */)
 {
 	m_logHandler = handler;
 }
 
+/**
+ * @brief 停止日志系统
+ * 
+ * 释放日志系统相关资源，包括关闭所有日志器和释放模式映射
+ */
 void WTSLogger::stop()
 {
 	m_bStopped = true;
@@ -259,6 +314,11 @@ void WTSLogger::stop()
 	spdlog::shutdown();
 }
 
+/**
+ * @brief 调试级别日志输出的实现函数
+ * @param logger 日志器指针，用于实际输出日志
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::debug_imp(SpdLoggerPtr logger, const char* message)
 {
 	if (logger)
@@ -271,6 +331,11 @@ void WTSLogger::debug_imp(SpdLoggerPtr logger, const char* message)
 		m_logHandler->handleLogAppend(LL_DEBUG, message);
 }
 
+/**
+ * @brief 信息级别日志输出的实现函数
+ * @param logger 日志器指针，用于实际输出日志
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::info_imp(SpdLoggerPtr logger, const char* message)
 {
 	if (logger)
@@ -283,6 +348,11 @@ void WTSLogger::info_imp(SpdLoggerPtr logger, const char* message)
 		m_logHandler->handleLogAppend(LL_INFO, message);
 }
 
+/**
+ * @brief 警告级别日志输出的实现函数
+ * @param logger 日志器指针，用于实际输出日志
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::warn_imp(SpdLoggerPtr logger, const char* message)
 {
 	if (logger)
@@ -295,6 +365,11 @@ void WTSLogger::warn_imp(SpdLoggerPtr logger, const char* message)
 		m_logHandler->handleLogAppend(LL_WARN, message);
 }
 
+/**
+ * @brief 错误级别日志输出的实现函数
+ * @param logger 日志器指针，用于实际输出日志
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::error_imp(SpdLoggerPtr logger, const char* message)
 {
 	if (logger)
@@ -307,6 +382,11 @@ void WTSLogger::error_imp(SpdLoggerPtr logger, const char* message)
 		m_logHandler->handleLogAppend(LL_ERROR, message);
 }
 
+/**
+ * @brief 致命错误级别日志输出的实现函数
+ * @param logger 日志器指针，用于实际输出日志
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::fatal_imp(SpdLoggerPtr logger, const char* message)
 {
 	if (logger)
@@ -319,6 +399,11 @@ void WTSLogger::fatal_imp(SpdLoggerPtr logger, const char* message)
 		m_logHandler->handleLogAppend(LL_FATAL, message);
 }
 
+/**
+ * @brief 直接输出指定级别的原始日志消息
+ * @param ll 日志级别，决定使用哪个日志函数来输出
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::log_raw(WTSLogLevel ll, const char* message)
 {
 	if (m_logLevel > ll || m_bStopped)
@@ -352,6 +437,12 @@ void WTSLogger::log_raw(WTSLogLevel ll, const char* message)
 	}
 }
 
+/**
+ * @brief 按分类直接输出指定级别的原始日志消息
+ * @param catName 分类名称，用于获取对应的日志器
+ * @param ll 日志级别，决定使用哪个日志函数来输出
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::log_raw_by_cat(const char* catName, WTSLogLevel ll, const char* message)
 {
 	if (m_logLevel > ll || m_bStopped)
@@ -394,6 +485,13 @@ void WTSLogger::log_raw_by_cat(const char* catName, WTSLogLevel ll, const char* 
 	}	
 }
 
+/**
+ * @brief 使用动态日志模式直接输出指定级别的日志消息
+ * @param patttern 日志模式，影响日志输出的格式和目标
+ * @param catName 分类名称，用于获取或创建对应的日志器
+ * @param ll 日志级别，决定使用哪个日志函数来输出
+ * @param message 要输出的日志消息
+ */
 void WTSLogger::log_dyn_raw(const char* patttern, const char* catName, WTSLogLevel ll, const char* message)
 {
 	if (m_logLevel > ll || m_bStopped)
@@ -434,6 +532,16 @@ void WTSLogger::log_dyn_raw(const char* patttern, const char* catName, WTSLogLev
 }
 
 
+/**
+ * @brief 获取指定名称的日志器，如果不存在且指定了模式则动态创建
+ * 
+ * 尝试获取指定名称的日志器，如果不存在且指定了有效的模式，
+ * 则根据模式和名称动态创建一个新的日志器
+ * 
+ * @param logger 要获取的日志器名称
+ * @param pattern 日志模式名称，默认为空字符串
+ * @return SpdLoggerPtr 返回找到或创建的日志器指针，如果失败则返回NULL
+ */
 SpdLoggerPtr WTSLogger::getLogger(const char* logger, const char* pattern /* = "" */)
 {
 	SpdLoggerPtr ret = spdlog::get(logger);
@@ -457,6 +565,12 @@ SpdLoggerPtr WTSLogger::getLogger(const char* logger, const char* pattern /* = "
 	return ret;
 }
 
+/**
+ * @brief 释放所有动态创建的日志器
+ * 
+ * 遍历m_setDynLoggers集合中所有记录的动态日志器名称，
+ * 通过spdlog::drop方法释放对应的日志器资源
+ */
 void WTSLogger::freeAllDynLoggers()
 {
 	for(const std::string& logger : m_setDynLoggers)
--- src/WTSTools/WTSLogger.h
+++ src/WTSTools/WTSLogger.h
@@ -1,4 +1,4 @@
-﻿/*!
+/*!
  * \file WTSLogger.h
  * \project	WonderTrader
  *
@@ -6,6 +6,10 @@
  * \date 2020/03/30
  * 
  * \brief 日志模块定义
+ *
+ * 本文件定义了WonderTrader的日志系统，基于spdlog实现
+ * 支持多种日志级别，多种日志输出方式，包括控制台、文件、每日滚动文件等
+ * 可以通过配置文件定制不同分类的日志输出格式和方式
  */
 #pragma once
 #include "../Includes/WTSTypes.h"
@@ -34,40 +38,125 @@ USING_NS_WTP;
 #define MAX_LOG_BUF_SIZE 2048
 
 
+/**
+ * @brief WonderTrader的日志系统类
+ *
+ * 基于spdlog实现的日志系统，提供了多种日志级别的输出方式
+ * 包括基本的调试、信息、警告、错误和致命错误等多级别日志
+ * 支持日志分类、动态日志和定制日志格式
+ * 该类的所有方法都是静态的，不需要创建类的实例
+ */
 class WTSLogger
 {
 private:
+	/**
+	 * @brief 调试级别日志实现函数
+	 * @param logger 日志器指针
+	 * @param message 日志消息
+	 */
 	static void debug_imp(SpdLoggerPtr logger, const char* message);
+
+	/**
+	 * @brief 信息级别日志实现函数
+	 * @param logger 日志器指针
+	 * @param message 日志消息
+	 */
 	static void info_imp(SpdLoggerPtr logger, const char* message);
+
+	/**
+	 * @brief 警告级别日志实现函数
+	 * @param logger 日志器指针
+	 * @param message 日志消息
+	 */
 	static void warn_imp(SpdLoggerPtr logger, const char* message);
+
+	/**
+	 * @brief 错误级别日志实现函数
+	 * @param logger 日志器指针
+	 * @param message 日志消息
+	 */
 	static void error_imp(SpdLoggerPtr logger, const char* message);
+
+	/**
+	 * @brief 致命错误级别日志实现函数
+	 * @param logger 日志器指针
+	 * @param message 日志消息
+	 */
 	static void fatal_imp(SpdLoggerPtr logger, const char* message);
 
+	/**
+	 * @brief 初始化指定分类的日志器
+	 * @param catName 日志分类名称
+	 * @param cfgLogger 日志器配置
+	 */
 	static void initLogger(const char* catName, WTSVariant* cfgLogger);
+
+	/**
+	 * @brief 获取指定名称的日志器
+	 * @param logger 日志器名称
+	 * @param pattern 日志模式，默认为空字符串
+	 * @return SpdLoggerPtr 日志器指针，如果不存在且模式为空则返回null
+	 */
 	static SpdLoggerPtr getLogger(const char* logger, const char* pattern = "");
 
+	/**
+	 * @brief 打印消息到控制台
+	 * @param buffer 消息内容
+	 */
 	static void print_message(const char* buffer);
 
 public:
-	/*
-	 *	直接输出
+	/**
+	 * @brief 直接输出日志消息
+	 * 
+	 * 使用默认的日志器输出指定级别的日志消息
+	 * 
+	 * @param ll 日志级别
+	 * @param message 日志消息
 	 */
 	static void log_raw(WTSLogLevel ll, const char* message);
 
-	/*
-	 *	分类输出
+	/**
+	 * @brief 按分类输出日志消息
+	 * 
+	 * 使用指定分类的日志器输出指定级别的日志消息
+	 * 
+	 * @param catName 分类名称
+	 * @param ll 日志级别
+	 * @param message 日志消息
 	 */
 	static void log_raw_by_cat(const char* catName, WTSLogLevel ll, const char* message);
 
-	/*
-	 *	动态分类输出
+	/**
+	 * @brief 使用动态分类模式输出日志消息
+	 * 
+	 * 使用指定模式和分类的日志器输出指定级别的日志消息
+	 * 
+	 * @param patttern 日志模式
+	 * @param catName 分类名称
+	 * @param ll 日志级别
+	 * @param message 日志消息
 	 */
 	static void log_dyn_raw(const char* patttern, const char* catName, WTSLogLevel ll, const char* message);
 
 
 //////////////////////////////////////////////////////////////////////////
-//fmt::format风格接口
+/**
+ * @brief fmt格式化日志接口
+ *
+ * 以下方法使用fmt格式化库提供格式化日志输出能力
+ * 支持多种数据类型的格式化并输出为日志
+ */
 public:
+	/**
+	 * @brief 输出调试级别日志
+	 * 
+	 * 使用fmt格式化库将参数格式化后输出调试级别日志
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void debug(const char* format, const Args& ...args)
 	{
@@ -85,6 +174,15 @@ public:
 		debug_imp(m_rootLogger, m_buffer);
 	}
 
+	/**
+	 * @brief 输出信息级别日志
+	 * 
+	 * 使用fmt格式化库将参数格式化后输出信息级别日志
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void info(const char* format, const Args& ...args)
 	{
@@ -102,6 +200,15 @@ public:
 		info_imp(m_rootLogger, m_buffer);
 	}
 
+	/**
+	 * @brief 输出警告级别日志
+	 * 
+	 * 使用fmt格式化库将参数格式化后输出警告级别日志
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void warn(const char* format, const Args& ...args)
 	{
@@ -119,6 +226,15 @@ public:
 		warn_imp(m_rootLogger, m_buffer);
 	}
 
+	/**
+	 * @brief 输出错误级别日志
+	 * 
+	 * 使用fmt格式化库将参数格式化后输出错误级别日志
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void error(const char* format, const Args& ...args)
 	{
@@ -136,6 +252,15 @@ public:
 		error_imp(m_rootLogger, m_buffer);
 	}
 
+	/**
+	 * @brief 输出致命错误级别日志
+	 * 
+	 * 使用fmt格式化库将参数格式化后输出致命错误级别日志
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void fatal(const char* format, const Args& ...args)
 	{
@@ -153,6 +278,16 @@ public:
 		fatal_imp(m_rootLogger, m_buffer);
 	}
 
+	/**
+	 * @brief 使用指定级别输出格式化日志
+	 * 
+	 * 将格式化参数按指定级别输出日志，使用默认日志器
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param ll 日志级别
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void log(WTSLogLevel ll, const char* format, const Args& ...args)
 	{
@@ -164,6 +299,17 @@ public:
 		log_raw(ll, m_buffer);
 	}
 
+	/**
+	 * @brief 使用指定分类和级别输出格式化日志
+	 * 
+	 * 根据分类名称和级别格式化输出日志
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param catName 日志分类名称
+	 * @param ll 日志级别
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void log_by_cat(const char* catName, WTSLogLevel ll, const char* format, const Args& ...args)
 	{
@@ -175,6 +321,17 @@ public:
 		log_raw_by_cat(catName, ll, m_buffer);
 	}
 
+	/**
+	 * @brief 使用指定分类和级别输出格式化日志，并添加分类前缀
+	 * 
+	 * 根据分类名称和级别格式化输出日志，日志内容前会添加[catName]前缀
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param catName 日志分类名称
+	 * @param ll 日志级别
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void log_by_cat_prefix(const char* catName, WTSLogLevel ll, const char* format, const Args& ...args)
 	{
@@ -189,6 +346,18 @@ public:
 		log_raw_by_cat(catName, ll, m_buffer);
 	}
 
+	/**
+	 * @brief 使用动态日志模式和分类输出格式化日志
+	 * 
+	 * 根据指定的日志模式、分类名称和级别格式化输出日志
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param patttern 日志模式
+	 * @param catName 日志分类名称
+	 * @param ll 日志级别
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void log_dyn(const char* patttern, const char* catName, WTSLogLevel ll, const char* format, const Args& ...args)
 	{
@@ -200,6 +369,18 @@ public:
 		log_dyn_raw(patttern, catName, ll, m_buffer);
 	}
 
+	/**
+	 * @brief 使用动态日志模式和分类输出格式化日志，并添加分类前缀
+	 * 
+	 * 根据指定的日志模式、分类名称和级别格式化输出日志，日志内容前会添加[catName]前缀
+	 * 
+	 * @tparam Args 参数类型列表
+	 * @param patttern 日志模式
+	 * @param catName 日志分类名称
+	 * @param ll 日志级别
+	 * @param format 格式化字符串
+	 * @param args 参数列表
+	 */
 	template<typename... Args>
 	static void log_dyn_prefix(const char* patttern, const char* catName, WTSLogLevel ll, const char* format, const Args& ...args)
 	{
@@ -216,12 +397,38 @@ public:
 	}
 
 public:
+	/**
+	 * @brief 初始化日志系统
+	 * 
+	 * 根据配置文件初始化日志系统，设置日志级别、输出目标等
+	 * 
+	 * @param propFile 配置文件路径，默认为"logcfg.json"
+	 * @param isFile 是否为文件路径，为false时propFile为配置内容字符串
+	 * @param handler 自定义日志处理器，默认为NULL
+	 */
 	static void init(const char* propFile = "logcfg.json", bool isFile = true, ILogHandler* handler = NULL);
 
+	/**
+	 * @brief 注册自定义日志处理器
+	 * 
+	 * 允许外部注册自定义日志处理器来处理日志消息
+	 * 
+	 * @param handler 自定义日志处理器，默认为NULL
+	 */
 	static void registerHandler(ILogHandler* handler = NULL);
 
+	/**
+	 * @brief 停止日志系统
+	 * 
+	 * 停止所有日志输出并释放相关资源
+	 */
 	static void stop();
 
+	/**
+	 * @brief 释放所有动态创建的日志器
+	 * 
+	 * 遍历并释放所有通过动态模式创建的日志器资源
+	 */
 	static void freeAllDynLoggers();
 
 private:
