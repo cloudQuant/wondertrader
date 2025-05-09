447a77f4df32a24e1b732a97da0cfe4bfdf4fd66
cloudQuant <yunjinqi@gmail.com>
2025-05-09 12:35:10 +0800

    ActionPolicyMgr

--- src/WtUftCore/ActionPolicyMgr.cpp
+++ src/WtUftCore/ActionPolicyMgr.cpp
@@ -1,11 +1,14 @@
-﻿/*!
+/*!
  * \file ActionPolicyMgr.cpp
  * \project	WonderTrader
  *
  * \author Wesley
  * \date 2020/03/30
  * 
- * \brief 
+ * \brief 交易动作策略管理器实现
+ *
+ * 本文件实现了交易动作策略管理器，负责从配置文件中加载
+ * 交易动作规则，并为交易执行器提供品种的交易动作规则查询功能。
  */
 #include "ActionPolicyMgr.h"
 
@@ -17,39 +20,68 @@
 
 USING_NS_WTP;
 
+/**
+ * @brief 构造函数
+ * 
+ * 初始化交易动作策略管理器
+ */
 ActionPolicyMgr::ActionPolicyMgr()
 {
 }
 
 
+/**
+ * @brief 析构函数
+ * 
+ * 清理交易动作策略管理器分配的资源
+ */
 ActionPolicyMgr::~ActionPolicyMgr()
 {
 }
 
+/**
+ * @brief 初始化动作策略管理器
+ * 
+ * 从配置文件中加载交易动作规则，并建立品种与规则组之间的映射关系
+ * 配置文件包含多个规则组，每个规则组包含多个交易动作规则及其应用的品种筛选器
+ * 
+ * @param filename 配置文件路径
+ * @return bool 初始化是否成功，true表示成功，false表示失败
+ */
 bool ActionPolicyMgr::init(const char* filename)
 {
+	// 从配置文件加载数据
 	WTSVariant* cfg = WTSCfgLoader::load_from_file(filename);
 	if (cfg == NULL)
 		return false;
 
+	// 遍历配置文件中的所有规则组
 	auto keys = cfg->memberNames();
 	for (auto it = keys.begin(); it != keys.end(); it++)
 	{
+		// 获取规则组名称
 		const char* gpName = (*it).c_str();
 		WTSVariant*	vGpItem = cfg->get(gpName);
+		// 为每个规则组创建一个全新的ActionRuleGroup
 		ActionRuleGroup& gp = _rules[gpName];
 
+		// 加载该规则组的所有交易动作规则
 		WTSVariant* vOrds = vGpItem->get("order");
 		if(vOrds != NULL && vOrds->isArray())
 		{
+			// 遍历处理每一个交易动作规则
 			for (uint32_t i = 0; i < vOrds->size(); i++)
 			{
 				WTSVariant* vObj = vOrds->get(i);
 				ActionRule aRule;
+				
+				// 从配置中提取动作类型和限制值
 				const char* action = vObj->getCString("action");
 				uint32_t uLimit = vObj->getUInt32("limit");
 				uint32_t uLimitS = vObj->getUInt32("limit_s");
 				uint32_t uLimitL = vObj->getUInt32("limit_l");
+				
+				// 将字符串动作类型转换为ActionType枚举值
 				if (wt_stricmp(action, "open") == 0)
 					aRule._atype = AT_Open;
 				else if (wt_stricmp(action, "close") == 0)
@@ -60,52 +92,73 @@ bool ActionPolicyMgr::init(const char* filename)
 					aRule._atype = AT_CloseYestoday;
 				else 
 				{
+					// 如果动作类型不识别，记录错误并跳过该条规则
 					WTSLogger::error("Loading action policy failed: unrecognized type {}", action);
 					continue;
 				}
 
-				aRule._limit = uLimit;
-				aRule._limit_s = uLimitS;
-				aRule._limit_l = uLimitL;
-				aRule._pure = vObj->getBoolean("pure");
+				// 设置规则的限制参数
+				aRule._limit = uLimit;    // 总手数限制
+				aRule._limit_s = uLimitS; // 空头限制
+				aRule._limit_l = uLimitL; // 多头限制
+				aRule._pure = vObj->getBoolean("pure"); // 是否为纯净模式
+				
+				// 将规则添加到规则组中
 				gp.emplace_back(aRule);
 			}
 		}
 
+		// 加载该规则组适用的品种过滤器
 		WTSVariant* filters = vGpItem->get("filters");
 		if(filters!=NULL && filters->isArray() && filters->size()>0)
 		{
+			// 遍历处理每一个品种代码
 			for (uint32_t i = 0; i < filters->size(); i++)
 			{
+				// 将品种代码与规则组名称建立映射关系
 				const char* commid = filters->get(i)->asCString();
 				_comm_rule_map[commid] = gpName;
 			}
 		}
 	}
 
+	// 释放配置对象内存
 	cfg->release();
 	return true;
 }
 
+/**
+ * @brief 获取指定品种的交易动作规则组
+ * 
+ * 根据品种代码查找相应的交易动作规则组
+ * 如果未找到指定品种的规则组，则使用默认组
+ * 
+ * @param pid 品种代码
+ * @return const ActionRuleGroup& 交易动作规则组引用
+ */
 const ActionRuleGroup& ActionPolicyMgr::getActionRules(const char* pid)
 {
+	// 默认使用"default"规则组
 	std::string gpName = "default";
 
-	{//先找到品种对应的规则组名称
+	{// 先找到品种对应的规则组名称
 		auto it = _comm_rule_map.find(pid);
 		if (it != _comm_rule_map.end())
-			gpName = it->second;
+			gpName = it->second; // 如果找到对应的规则组，则使用该规则组
 	}
 
 	{
+		// 根据规则组名称查找规则组
 		auto it = _rules.find(gpName);
 		if (it == _rules.end())
 		{
+			// 如果指定的规则组不存在，则使用默认规则组
 			it = _rules.find("default");
 			WTSLogger::error("Action policy group {} not exists, changed to default group", gpName.c_str());
 		}
 
+		// 断言规则组必定存在，这是一个程序防御机制
 		assert(it != _rules.end());
-		return it->second;
+		return it->second; // 返回找到的规则组
 	}
 }
--- src/WtUftCore/ActionPolicyMgr.h
+++ src/WtUftCore/ActionPolicyMgr.h
@@ -1,11 +1,14 @@
-﻿/*!
+/*!
  * \file ActionPolicyMgr.h
  * \project	WonderTrader
  *
  * \author Wesley
  * \date 2020/03/30
  * 
- * \brief 
+ * \brief 交易动作策略管理器
+ *
+ * 该文件定义了WonderTrader中的交易动作策略管理器，用于管理不同品种的交易动作规则，
+ * 实现对开平仓动作的精细化控制，包括数量限制、多空方向限制等功能。
  */
 #pragma once
 #include <vector>
@@ -18,47 +21,98 @@
 NS_WTP_BEGIN
 class WTSVariant;
 
+/**
+ * @brief 交易动作类型枚举
+ * 
+ * 定义了交易系统中支持的各种交易动作类型，包括开仓、平仓、平今和平昨
+ */
 typedef enum tagActionType
 {
-	AT_Unknown = 8888,
-	AT_Open = 9999,		//开仓
-	AT_Close,			//平仓
-	AT_CloseToday,		//平今
-	AT_CloseYestoday	//平昨
+	AT_Unknown = 8888,    /**< 未知动作类型 */
+	AT_Open = 9999,		/**< 开仓动作 */
+	AT_Close,			/**< 平仓动作（不区分平今平昨） */
+	AT_CloseToday,		/**< 平今仓动作 */
+	AT_CloseYestoday	/**< 平昨仓动作 */
 } ActionType;
 
+/**
+ * @brief 交易动作规则结构体
+ * 
+ * 定义了一条交易动作规则，包括动作类型、数量限制、方向限制等
+ */
 typedef struct _ActionRule
 {
-	ActionType	_atype;		//动作类型
-	uint32_t	_limit;		//手数限制
-	uint32_t	_limit_l;	//多头手数限制
-	uint32_t	_limit_s;	//空头手数限制
-	bool		_pure;		//主要针对AT_CloseToday和AT_CloseYestoday，用于判断是否是净今仓或者净昨仓
+	ActionType	_atype;		/**< 动作类型，如开仓、平仓等 */
+	uint32_t	_limit;		/**< 总手数限制，不区分方向 */
+	uint32_t	_limit_l;	/**< 多头方向手数限制 */
+	uint32_t	_limit_s;	/**< 空头方向手数限制 */
+	bool		_pure;		/**< 是否为纯净模式，主要针对AT_CloseToday和AT_CloseYestoday，用于判断是否是净今仓或者净昨仓 */
 
+	/**
+	 * @brief 构造函数
+	 * 
+	 * 将结构体所有成员初始化为0
+	 */
 	_ActionRule()
 	{
 		memset(this, 0, sizeof(_ActionRule));
 	}
 } ActionRule;
 
+/**
+ * @brief 交易动作规则组
+ * 
+ * 定义了一组交易动作规则，一个交易品种或品种组可以关联一个规则组
+ */
 typedef std::vector<ActionRule>	ActionRuleGroup;
 
+/**
+ * @brief 交易动作策略管理器
+ * 
+ * 管理交易系统中不同品种的交易动作规则，负责从配置文件加载规则
+ * 并为交易执行器提供对应品种的交易动作规则
+ */
 class ActionPolicyMgr
 {
 public:
+	/**
+	 * @brief 构造函数
+	 */
 	ActionPolicyMgr();
+
+	/**
+	 * @brief 析构函数
+	 */
 	~ActionPolicyMgr();
 
 public:
+	/**
+	 * @brief 初始化动作策略管理器
+	 * 
+	 * 从指定的配置文件中加载交易动作规则
+	 * 
+	 * @param filename 配置文件路径
+	 * @return bool 初始化是否成功
+	 */
 	bool init(const char* filename);
 
+	/**
+	 * @brief 获取指定品种的交易动作规则组
+	 * 
+	 * 根据品种代码查找相应的交易动作规则组，如果未找到则返回默认组
+	 * 
+	 * @param pid 品种代码
+	 * @return const ActionRuleGroup& 交易动作规则组引用
+	 */
 	const ActionRuleGroup& getActionRules(const char* pid);
 
 private:
+	/** @brief 规则组映射表类型定义 */
 	typedef wt_hashmap<std::string, ActionRuleGroup> RulesMap;
-	RulesMap	_rules;	//规则表
+	
+	RulesMap	_rules;	/**< 规则组映射表，键为规则组名称，值为规则组 */
 
-	wt_hashmap<std::string, std::string> _comm_rule_map;	//品种规则映射
+	wt_hashmap<std::string, std::string> _comm_rule_map;	/**< 品种与规则组映射表，键为品种代码，值为规则组名称 */
 };
 
 NS_WTP_END
