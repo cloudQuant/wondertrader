c1ba293e4e68078ac33279729f3c52044f7f816e
cloudQuant <yunjinqi@gmail.com>
2025-05-09 12:06:57 +0800

    WTLMDB

--- src/WTSUtils/SignalHook.hpp
+++ src/WTSUtils/SignalHook.hpp
@@ -1,9 +1,45 @@
-﻿#pragma once
+/*!
+ * \file SignalHook.hpp
+ * \project	WonderTrader
+ *
+ * \author Wesley
+ * \date 2020/03/30
+ * 
+ * \brief 信号处理钩子模块
+ *
+ * 本文件实现了对系统信号的捕获和处理机制，包括崩溃信号、终止信号等。
+ * 提供了信号处理函数和安装信号钩子的功能，支持Windows和Linux平台。
+ * 可以通过自定义回调函数来处理系统发出的各种信号，并在需要时输出堆栈跟踪信息。
+ */
+#pragma once
 #include <signal.h>
 #include "./StackTracer/StackTracer.h"
+
+/**
+ * @brief 信号日志回调函数
+ * 
+ * 全局变量，用于处理信号发生时的日志记录
+ * 由install_signal_hooks函数设置，由handle_signal函数调用
+ */
 TracerLogCallback g_cbSignalLog = NULL;
+
+/**
+ * @brief 信号退出处理函数
+ * 
+ * 全局变量，用于处理信号发生时的自定义退出操作
+ * 如果设置了此函数，则优先使用此函数处理退出操作，而不是简单调用exit()
+ */
 ExitHandler g_exitHandler = NULL;
 
+/**
+ * @brief 处理系统信号的函数
+ * 
+ * 此函数是处理各种系统信号的回调函数，在信号发生时被调用
+ * 根据不同的信号类型执行不同的处理操作，包括日志记录、堆栈跟踪和退出处理
+ * 对于Windows和Linux系统提供了不同的信号处理逻辑
+ * 
+ * @param signum 信号编号，指示发生的系统信号类型
+ */
 void handle_signal(int signum)
 {
 	static char buf[64] = { 0 };
@@ -145,6 +181,16 @@ void handle_signal(int signum)
 	}
 }
 
+/**
+ * @brief 安装系统信号处理钩子
+ * 
+ * 此函数用于设置信号处理机制，将所有系统信号关联到handle_signal函数
+ * 设置日志记录回调函数和可选的退出处理函数
+ * 调用此函数后，程序将能够捕获并处理各种系统信号
+ * 
+ * @param cbLog 必需的日志回调函数，用于记录信号相关的日志信息
+ * @param sigHandler 可选的信号退出处理函数，如果提供，将在用于处理需要终止程序的信号
+ */
 void install_signal_hooks(TracerLogCallback cbLog, ExitHandler sigHandler = NULL)
 {
 	g_cbSignalLog = cbLog;
